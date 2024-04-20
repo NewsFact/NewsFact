203aaed9b2b5c133ab3ca986c1a95b4395655177
mrhcjones <150591975+TheHCJ@users.noreply.github.com>
2024-04-20 22:14:28 +0100

    move: stuff into header

--- lib/card/_card.dart
+++ lib/card/_card.dart
@@ -5,6 +5,7 @@ import 'package:newsfact/utils/favicon.dart';
 import 'package:universal_feed/universal_feed.dart' show Item;
 import 'package:url_launcher/url_launcher.dart';
 import 'package:timeago/timeago.dart' as timeago;
+import 'package:url_launcher/url_launcher_string.dart';
 
 class NewsCard extends StatefulWidget {
   final Item item;
@@ -19,9 +20,19 @@ class LargeCardState extends State<NewsCard> {
   Widget build(BuildContext context) {
     final item = widget.item;
 
-        return Card(child: Padding(
-          padding: EdgeInsets.all(8),
-          child: Column(
+        return Card(child: 
+          ListTile(
+            onTap: () => launchUrlString(item.link!.href),
+            title: Row(children: [
+              FaviconImage(item.link!.href.toString(), width: 32,),
+              Text(item.authors.toString(),
+                      style: Theme.of(context).textTheme.labelMedium),
+                  const SizedBox(width: 8.0),
+                  if(item.published != null && (parseDateTime(item.published!.value) as DateTime?) != null)
+                  Text(timeago.format(parseDateTime(item.published!.value)),
+                      style: Theme.of(context).textTheme.labelMedium)
+            ],),
+            subtitle: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               if(item.image != null)
@@ -29,18 +40,7 @@ class LargeCardState extends State<NewsCard> {
                 child: Image.network(item.image!.url, width: 100,),
                 borderRadius: BorderRadius.all(Radius.circular(16)),
               ),
-              Row(
-                children: [
-                  if (item.link != null)
-                  FaviconImage(item.link!.href.toString(), width: 32,), SizedBox(width: 5),
-                  Text(item.authors.toString(),
-                      style: Theme.of(context).textTheme.labelMedium),
-                  SizedBox(width: 5),
-                  if(item.published != null && (parseDateTime(item.published!.value) as DateTime?) != null)
-                  Text(timeago.format(parseDateTime(item.published!.value)),
-                      style: Theme.of(context).textTheme.labelMedium)
-                ],
-              ),
+              
               if (item.title != null)
               Text(item.title!, style: Theme.of(context).textTheme.titleMedium, maxLines: 2, overflow: TextOverflow.ellipsis,),
               if (item.description != null)
